9a65a452c77c99271b61f3bc0d8eda5b5f6195b5
jvanhook13 <jaredvanhook1313@gmail.com>
2020-04-15 23:15:44 -0700

    manager view is done. Every function works on its own besides adding inventory but added code explaining

--- bamazonmanager.js
+++ bamazonmanager.js
@@ -19,6 +19,10 @@ var connection = mysql.createConnection({
 });
 
 
+var invenADD;
+var selectedItem;
+
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
@@ -30,51 +34,201 @@ connection.connect(function (err) {
 
 inquirer.prompt(
 
-    [{
-            name: "item",
-            type: "list",
-            message: "What are we doing today?",
-            choices: ['View Products', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+        [{
+                name: "item",
+                type: "list",
+                message: "What are we doing today?",
+                choices: ['View Products', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
 
-        },
+            },
 
-    ]
-
-
-).then( function (answer) {
-        
-    console.log(answer, "this one")
+        ]
 
 
+    ).then(function (answer) {
 
-        switch (answer.item) {
+            console.log(answer, "this one")
 
 
 
-            case ('View Products'):
-
-                connection.query("SELECT * FROM products", function (err, res) {
-                    if (err) throw err;
+            switch (answer.item) {
 
 
-                    {console.table(res);}
+
+                case ('View Products'):
+
+                    connection.query("SELECT * FROM products", function (err, res) {
+                        if (err) throw err;
 
 
-                })
-
-                break;
-
-            case('View Low Inventory'):
-            
-            connection.query("SELECT * FROM products WHERE quantity BETWEEN ? and ? ", [0, 45], function(err, res) {
-
-                if(err) throw err ;
-
-                {console.table(res);}
+                        {
+                            console.table(res);
+                        }
 
 
-            })
+                    })
+                    connection.end()
+                    break;
+
+                case ('View Low Inventory'):
+
+                    connection.query("SELECT * FROM products WHERE quantity BETWEEN ? and ? ", [0, 45], function (err, res) {
+
+                        if (err) throw err;
+
+                        {
+                            console.table(res);
+                        }
 
 
-        }
-    })
+
+                        connection.end()
+                    })
+                    break;
+
+
+
+                    // case ('Add to Inventory'):
+
+                    //     inquirer.prompt(
+
+                    //         [{
+
+                    //                 name: "item",
+                    //                 type: "input",
+                    //                 message: "Which product are we adding to?",
+
+
+                    //             },
+
+                    //             {
+                    //                 name: "amount",
+                    //                 type: "input",
+                    //                 message: "How much would you like to add?"
+
+
+                    //             }
+
+                    //         ]).then(function (answer) {
+
+                    //         var selectedItem = answer.item
+                    //         invenADD = answer.amount
+
+                    //         connection.query("SELECT * FROM products WHERE prodID=?", [selectedItem], function (err, res) {
+                    //             if (err) throw err;
+
+                    //             {
+
+                    //                 var oldAmount = res[0].quantity
+
+
+
+                    //                 console.log(invenADD , "what are you")
+
+
+                    //                 function newAmount (oldAmount, invenADD) {
+                    //                     if (invenADD == 0) {
+                    //                         return oldAmount;
+                    //                     } else {
+                    //                         newAmount(oldAmount ^ invenADD, (oldAmount & invenADD) << 1)
+
+                    //                         connection.query("UPDATE products SET quantity=? WHERE prodID=?", [newAmount, selectedItem], function (err, res) {
+
+                    //                             if (err) throw err ;
+
+
+                    //                         })
+
+                    //                         connection.query("SELECT * FROM products", function (err, res) {
+
+                    //                             if (err) throw err ;
+
+                    //                             {console.table(res)}
+
+
+                    //                         })
+
+                    //                     }
+                    //                 };
+
+                    //                 newAmount()
+                    /// SOOOOO this works except that using the + concats my database rather actually adding it, so I tried to find some work arounds and tried a recursion loop but val.slice isnt a function error gets thrown and Im honestly lost
+
+                case ('Add New Product'):
+                    var prodName;
+                    var depName;
+                    var price;
+                    var quantity;
+
+
+                    inquirer.prompt([
+
+                        {
+
+                            name: "prod",
+                            type: "input",
+                            message: "What is the product name"
+
+                        },
+
+                        {
+
+                            name: "dep",
+                            type: "input",
+                            message: "What is the department name"
+
+                        },
+
+                        {
+
+                            name: "price",
+                            type: "input",
+                            message: "What is the product price"
+
+                        },
+
+                        {
+
+                            name: "quant",
+                            type: "input",
+                            message: "How much do we have?"
+
+                        }
+                    ]).then(function (answer) {
+
+                            prodName = answer.prod
+                            depName = answer.dep
+                            price = answer.price
+                            quantity = answer.quant
+                            var prodID ;
+
+
+                            connection.query("INSERT INTO products VALUE(? , ? , ? , ? , ?)", [prodID, prodName, depName, price, quantity], function (err, res) {
+
+                                    if (err) throw err;
+
+                                }),
+
+                                connection.query("SELECT * FROM products"),
+                                function (err, res) {
+
+                                    if (err) throw err;
+
+                                    {
+                                        console.table(res)
+                                    }
+
+                                
+
+                       
+
+                    }
+
+                  
+
+            }
+
+
+
+
+        )}})
